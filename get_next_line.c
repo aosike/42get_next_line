8f57d4883c0d15ecef51a1d8690325a064eae8c4
Aaron GUNDRY <agundry@e1z3r3p26.42.us.org>
2017-03-11 15:12:18 -0800

    aksjdhf

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agundry <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 11:49:11 by agundry           #+#    #+#             */
-/*   Updated: 2017/03/04 19:37:52 by agundry          ###   ########.fr       */
+/*   Updated: 2017/03/11 15:11:57 by agundry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,60 @@
 int	get_next_line(const int fd, char **line)
 {
 	static char	*fdstr[256];
-	char		buf[BUFF_SIZE + 1];
+	char		buf[BUFF_SIZE + 1]; ///might have to malloc in order to save lines :///
 	char		*front;
 	char		*back;
 	size_t		rsize;
 
 	ft_bzero(buf, (BUFF_SIZE + 1));
-	if (fdstr[fd] && *fdstr[fd])  //ft_strdel of fdstr??
+	if (fdstr[fd] && *fdstr[fd])
+	{
 		ft_strcpy(buf, fdstr[fd]);
-	rsize = 1;
-	while ((!line[fd] || !(ft_strchr(line[fd], '\n'))) && rsize > 0) //appears as though line[fd] will always exist. 																			fucking lame
+		ft_strdel(&fdstr[fd]);
+	}
+	rsize = BUFF_SIZE;
+	while (rsize > 0)
 	{
 		if ((back = ft_strchr(buf, '\n')))
 			fdstr[fd] = !(*(back + 1)) ? NULL : ft_strdup((back + 1));
 		else
 			back = ft_strchr(buf, '\0');
-		front = ft_strndup(buf, ((*back == '\n' ? (back + 1) : back) - buf)); //ft_strdel to front??
-		line[fd] = linegetter(line[fd], front);
-		ft_bzero(buf, BUFF_SIZE);
-		if (!(ft_strchr(line[fd], '\n')))
+		front = linegetter(front, buf, back);
+		if (*back != '\n')
+		{
+			ft_bzero(buf, (BUFF_SIZE + 1));      //would save lines here : see top
 			rsize = read(fd, buf, BUFF_SIZE);
+		}
+		else rsize = 0;
 	}
-	return (*line[fd] ? 1 : 0);	
+	*line = *front ? ft_strndup(front, (ft_strlen(front) - 1)) : NULL;
+	ft_strdel(&front);
+	return (*line ? 1 : 0);	
 }
 
-char	*linegetter(char *d, char *s)
+char	*linegetter(char *d, char *s, char *b)
 {
 	char	*tmp;
+	char	*tmp1;
 
+	tmp1 = ft_strndup(s, (((*b == '\n') ? (b + 1) : b) - s));
 	if (d != NULL)
 	{
 		tmp = ft_strdup(d);
 		if (*d)
 			ft_strdel(&d);
-		d = ft_strjoin(tmp, s);
-		ft_strdel(&tmp);
+		d = ft_strjoin(tmp, tmp1);
+		if (*tmp)
+			ft_strdel(&tmp);
+		if (*tmp1)
+			ft_strdel(&tmp1);
 		return (d);
 	}
-	else return (d = ft_strdup(s));
+	else
+	{
+		d = ft_strdup(tmp1);
+		if (*tmp1)
+			ft_strdel(&tmp1);
+		return (d);
+	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,26 +6,17 @@
 /*   By: agundry <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/02 20:59:34 by agundry           #+#    #+#             */
-/*   Updated: 2017/03/01 16:47:57 by agundry          ###   ########.fr       */
+/*   Updated: 2017/03/11 14:34:08 by agundry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 420
 # include <fcntl.h>
 # include "libft/libft.h"
 
-/*typedef struct		s_list
-{
-	void			*content;
-	size_t			content_size;
-	int				fd;
-	struct s_list	*next;
-}					t_list;
-*/
 int		get_next_line(const int fd, char **line);
-char	*linegetter(char *d, char *s);
-int		readwrap(int fd, char *buf, char *fdstr);
+char	*linegetter(char *d, char *s, char *b);
 
 #endif

288a442e32c9650e7c35b5fc8306cadfaf431d2a
Aaron GUNDRY <agundry@e1z3r5p18.42.us.org>
2017-03-21 17:48:56 -0700

    asldf

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agundry <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 16:00:03 by agundry           #+#    #+#             */
-/*   Updated: 2017/03/15 17:13:51 by agundry          ###   ########.fr       */
+/*   Updated: 2017/03/21 17:48:38 by agundry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,63 @@
 
 int	get_next_line(int fd, char **line)
 {
-	static char	keep[256][BUFF_SIZE];
-	char		buf[BUFF_SIZE + 1];
-	char		*b;
-	char		*f;
-	size_t		s;
+	static char keep[4864][BUFF_SIZE];
+	t_gnl		gnl;
 
-	ft_bzero(buf, BUFF_SIZE + 1); //VERY CLOSE - is adding newline to f.
-	s = BUFF_SIZE;
-	f = 0;
-	if (*keep[fd])
-		ft_strcpy(buf, keep[fd]);
-	else
-		s = read(fd, buf, BUFF_SIZE);
-	ft_bzero(keep[fd], BUFF_SIZE);
-	while (s > 0)
+	gnl.f = 0;
+	if (!gnlinit(&keep[fd], fd, &gnl, &line))
+		return (-1);
+	while (gnl.s > 0)
 	{
-		if ((b = ft_strchr(buf, '\n')))
-			ft_strcpy(keep[fd], b + 1);
-		else b = ft_strchr(buf, '\0');
-		ft_bzero(b + 1, ((BUFF_SIZE + 1) - (b - buf)));
-		f = linegetter(f, buf);
-		s = (*b != '\n') ? read(fd, buf, BUFF_SIZE) : 0;
+		if ((gnl.b = ft_strchr(gnl.buf, '\n')))
+			ft_strcpy(keep[fd], gnl.b + 1);
+		else
+			gnl.b = ft_strchr(gnl.buf, '\0');
+		ft_bzero(gnl.b + 1, ((BUFF_SIZE + 1) - (gnl.b - gnl.buf)));
+		gnl.f = linegetter(gnl.f, gnl.buf);
+		gnl.s = *(gnl.b) == '\n' ? 0 : read(fd, gnl.buf, BUFF_SIZE);
 	}
-	*line = f ? ft_strdup(f) : 0;
+	*line = *(gnl.f) ? ft_strdup(gnl.f) : 0; ///////////////////fuck
 	return (*line ? 1 : 0);
+}
+
+int	gnlinit(char (*keep)[BUFF_SIZE], int fd, t_gnl *gnl, char ***line)
+{
+
+	ft_bzero(gnl->buf, BUFF_SIZE + 1);
+	gnl->s = BUFF_SIZE;
+	if (fd < 0 || !line)
+		return (0);
+	if (*keep)
+		ft_strcpy(gnl->buf, *keep);
+	else
+		gnl->s = read(fd, gnl->buf, BUFF_SIZE);
+	if (gnl->s < 0)
+		return (0);
+	ft_bzero(*keep, BUFF_SIZE);
+	return (1);
 }
 
 char	*linegetter(char *d, char *s)
 {
 	char	*tmp;
+	char	*src;
 
+	if (ft_strrchr(s, '\n'))
+		src = ft_strndup(s, ft_strlen(s) - 1);
+	else
+		src = ft_strdup(s);
 	if (d != NULL)
 	{
 		tmp = ft_strdup(d);
 		ft_strdel(&d);
-		d = ft_strjoin(tmp, s);
+		d = ft_strjoin(tmp, src);
 		ft_strdel(&tmp);
+		ft_strdel(&src);
 		return (d);
 	}
-	else return (d = ft_strdup(s));
+	else
+		d = ft_strdup(src);
+	ft_strdel(&src);
+	return (d);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,25 @@
 /*   By: agundry <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/02 20:59:34 by agundry           #+#    #+#             */
-/*   Updated: 2017/03/11 14:34:08 by agundry          ###   ########.fr       */
+/*   Updated: 2017/03/21 17:33:57 by agundry          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 420
+# define BUFF_SIZE 42
 # include <fcntl.h>
 # include "libft/libft.h"
 
+typedef struct	s_gnl{
+	char		buf[BUFF_SIZE + 1];
+	int			s;
+	char		*f;
+	char		*b;
+}				t_gnl;
+
 int		get_next_line(const int fd, char **line);
-char	*linegetter(char *d, char *s, char *b);
+int		gnlinit(char (*keep)[BUFF_SIZE], int fd, t_gnl *gnl, char ***line);
+char	*linegetter(char *d, char *s);
 
 #endif
